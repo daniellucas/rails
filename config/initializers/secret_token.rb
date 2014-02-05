f2ab6b02ee1209dbe95bda2d80037679d9d9f446
Daniel Lucas <me@daniellucas.net>
2014-02-05 02:24:15 -0800

    bosta e cu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DanielApp::Application.config.secret_key_base = '0304554c330c25b02032cbad28d68d2daced4d31b1ba12f45ea9c3fc16ddc08c66ba3ab825538bba27e7f02d7a33f33f1f661e30d13215703e98ad7932fc7b96'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+DanielApp::Application.routes.draw do
+  get "welcome/index"
+  root 'welcome#index'
+end
